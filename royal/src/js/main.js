955268ce013fc8280c613a8f822ff4c2bb44c896
nargizaladdinli <nargizalieva05@gmail.com>
2023-08-07 02:19:54 +0400

    Edit page

--- royal/src/js/main.js
+++ royal/src/js/main.js
@@ -2,9 +2,13 @@
 
 const click = document.querySelectorAll('.click');
 const accordion = document.querySelectorAll(".down");
+const openin = document.querySelectorAll(".opening");
+const close = document.querySelectorAll(".close")
 
 click.forEach((item, index) => {
     item.addEventListener("click", () => {
+        close[index].classList.toggle("bur");
+        openin[index].classList.toggle("bir");
         accordion[index].classList.toggle('up');
         item.classList.toggle("open")
     })
